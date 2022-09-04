02a1edaf0e5334c579a761a95c663fcfa0c1a9a0
jyr <jyr@wavers.co.kr>
2022-09-04 19:17:53 +0900

    Get Status Updates From Workers

--- app/src/main/java/io/jyryuitpro/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/workmanagerdemo1/MainActivity.kt
@@ -2,6 +2,7 @@ package io.jyryuitpro.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import io.jyryuitpro.workmanagerdemo1.databinding.ActivityMainBinding
@@ -21,9 +22,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOneTimeWorkRequest() {
+        val workManager = WorkManager.getInstance(applicationContext)
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
             .build()
-        WorkManager.getInstance(applicationContext)
-            .enqueue(uploadRequest)
+        workManager.enqueue(uploadRequest)
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id)
+            .observe(this, Observer {
+                binding.textView.text = it.toString()
+            })
     }
 }
